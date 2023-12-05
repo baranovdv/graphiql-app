6df01b584a810bff7a8dcf3c671c62a3559c0202
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-05 23:49:06 +0700

    chore: add vitest

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import App from '../App';
+
+/* Test to test the tests. To be modified after adding any content in App */
+describe('App', () => {
+  it('Renders GraphQL', () => {
+    render(<App />);
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('GraphQL');
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable max-len */
+/* eslint-disable import/no-extraneous-dependencies */
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    global: 'window',
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+  envDir: './',
+});

2af2b68e76bdb3e5b17bd3b3760f79ecf87bd910
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-12-06 18:25:13 +0300

    fix:fix test

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/main.module.css';
 function WelcomePage() {
   return (
     <div className={styles.mainDiv}>
-      <h1> This is Welcome Page</h1>
+      <h1>This is Welcome Page</h1>
       <a href="/SignIn/">SignIn</a>
       <a href="/MainPage/">Main</a>
     </div>

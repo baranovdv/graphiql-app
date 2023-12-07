b4be9d256a7a20cee175bd3a5cd52b8ae3afd2b4
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-12-07 08:27:53 +0300

    fix:unlock pre push test hook and adjust test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,12 +1,11 @@
 import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
-import App from '../App';
+import WelcomePage from '../pages/WelcomePage';
 
 /* Test to test the tests. To be modified after adding any content in App */
 describe('App', () => {
   it('Renders GraphQL', () => {
-    render(<App />);
+    render(<WelcomePage />);
     expect(screen.getByText('This is Welcome Page')).toBeInTheDocument();
   });
 });

7fb80874c6871a4899683e07698dc682c5d4ecf2
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2023-12-08 23:53:11 +0300

    GT-31 <feat:add footer>

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import classes from './footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer className={classes.footer}>
+      <a href="https://github.com/baranovdv" className={classes.link}>
+        <img
+          className={classes.footerGithubLink}
+          src="../../public/github.svg"
+          alt="gitlogo"
+        />
+      </a>
+
+      <span className={classes.footerYear}>2023-2024</span>
+      <a href="https://rs.school/react/" className={classes.link}>
+        <img
+          className={classes.footerRssLink}
+          src="../../public/RSS_React_logo.png"
+          alt="Rslogo"
+        />
+      </a>
+    </footer>
+  );
+}

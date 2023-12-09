d7280ace2c766c71207442c8f4e18cbc11f03f9d
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-10 05:13:19 +0700

    feat: update test

--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -3,19 +3,22 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import Header from '../components/Header/Header';
+import { LocaleProvider } from '../context/StoreContext';
 
 /* Test to test the tests. To be modified after adding any content in App */
 describe('App', () => {
   it('Renders GraphQL', () => {
     render(
       <Provider store={store}>
-        <Header />
+        <LocaleProvider>
+          <Header />
+        </LocaleProvider>
       </Provider>
     );
     expect(
       screen.getByRole('button', {
         name: /выйти/i,
       })
-    ).toHaveTextContent('выйти');
+    ).toHaveTextContent('Выйти');
   });
 });

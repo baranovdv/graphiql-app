fd888b13cb519d89d1dfbf480036f5d335edf5c1
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-26 01:53:39 +0700

    feat: add Root docs components

---
+++ src/components/Docs/RootItem/RootItem.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { Fragment, useState } from 'react';
+import { ItemType2 } from '../../../interfaces/interfaces';
+import getArgType from '../../../utils/docsUtils/getArgType';
+import getReturnType from '../../../utils/docsUtils/getReturnType';
+import ItemLink from '../ItemLink/ItemLink';
+import classes from './RootItem.module.css';
+
+interface RootItemProps {
+  item: ItemType2;
+}
+
+export default function RootItem({ item }: RootItemProps) {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescHandler = () => setShowDescription(!showDescription);
+
+  return (
+    <div className={classes.item} key={item.name}>
+      {item.name}
+      {item.description && (
+        <span className={classes.descriptionLink} onClick={toggleDescHandler}>
+          {` [?] `}
+        </span>
+      )}
+      (
+      {item.args?.map((arg) => (
+        <Fragment key={arg.name}>
+          <ItemLink type={getArgType(arg)} title={arg.name} />
+          &#44;&nbsp;
+        </Fragment>
+      ))}
+      ):&nbsp;
+      <ItemLink type={getReturnType(item)} color="orange" />
+      {showDescription && (
+        <div className={classes.description}>{item.description}</div>
+      )}
+      <hr className={classes.hr} />
+    </div>
+  );
+}

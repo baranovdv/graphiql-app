540188a42f0d93aa835eaf6445651ae305dac500
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-27 19:16:22 +0700

    GT-62 <refactor: refactor styles>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,21 @@
-import classes from './footer.module.css';
+import classes from './Footer.module.css';
+import githubLogo from '../../assets/img/github.svg';
+import RSLogo from '../../assets/img/RSS_React_logo.png';
 
 export default function Footer() {
-  const page: string = window.location.pathname.split('/')[1];
   return (
-    <footer
-      className={classes.footer}
-      style={{
-        maxWidth: page === 'SignUp' || page === 'SignIn' ? 800 : 1520,
-      }}
-    >
+    <footer className={classes.footer}>
       <a href="https://github.com/baranovdv" className={classes.link}>
         <img
           className={classes.footerGithubLink}
-          src="../../public/github.svg"
+          src={githubLogo}
           alt="gitlogo"
         />
       </a>
 
       <span className={classes.footerYear}>2023-2024</span>
       <a href="https://rs.school/react/" className={classes.link}>
-        <img
-          className={classes.footerRssLink}
-          src="../../public/RSS_React_logo.png"
-          alt="Rslogo"
-        />
+        <img className={classes.footerRssLink} src={RSLogo} alt="Rslogo" />
       </a>
     </footer>
   );

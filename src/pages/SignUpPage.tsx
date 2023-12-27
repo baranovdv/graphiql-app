cf2df549665a046f8a9c5d8c94e43b35f5f02c36
baranovdv <baranov.d.v.1988@gmail.com>
2023-12-27 21:20:38 +0700

    refactor: refactor signUp page

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -3,32 +3,30 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Button, Stack } from '@mui/material';
+import {
+  PasswordElement,
+  PasswordRepeatElement,
+  TextFieldElement,
+} from 'react-hook-form-mui';
 import { Client } from '../interfaces/interfaces';
-import Label from '../components/label/Label';
-import Input from '../components/input/Input';
-import { FieldsForRegistration, UserSchema } from '../utils/utils';
 import classes from '../styles/SingUp.module.css';
 import { useLocale } from '../context/StoreContext';
 import { registerWithEmailAndPassword, auth } from '../firebase';
+import RegistrationSchema from '../data/validationScheme/registrationSchema';
 
 function SignUpPage() {
   const { strings } = useLocale();
 
-  const form = useForm({
-    defaultValues: async () => {
-      return {
-        username: '',
-        email: '',
-        firstPassword: '',
-        secondPassword: '',
-      };
-    },
-    resolver: yupResolver(UserSchema()),
-    mode: 'onChange',
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm({
+    mode: 'all',
+    resolver: yupResolver(RegistrationSchema()),
   });
 
-  const { register, handleSubmit, formState } = form;
-  const { errors, isValid } = formState;
   const [user, loading] = useAuthState(auth);
   const registerUser = (data: Client) => {
     registerWithEmailAndPassword(data.username, data.email, data.firstPassword);
@@ -41,34 +39,65 @@ function SignUpPage() {
   }, [user, loading]);
 
   return (
-    <main className={classes.main}>
+    <section className={classes.section}>
       <h1 className={classes.title}>{strings.singup_page_title}</h1>
       <form
         className={classes.form}
         onSubmit={handleSubmit(registerUser)}
         noValidate
       >
-        {FieldsForRegistration().map((field) => (
-          <Label className={classes.field} htmlFor={field.id} key={field.id}>
-            {field.label}
-            <div className={classes.wrapperInput}>
-              <Input {...field} register={register} />
-              {errors[field.id as keyof Client]?.message && (
-                <p className={classes.error}>
-                  {errors[field.id as keyof Client]?.message}
-                </p>
-              )}
-            </div>
-          </Label>
-        ))}
-        <input
-          className={classes.button}
-          type="submit"
-          value={strings.signup_button}
-          disabled={!isValid}
-        />
+        <Stack
+          className="flex flex-col justify-center"
+          spacing={2}
+          sx={{ minWidth: '320px' }}
+        >
+          <TextFieldElement
+            name="username"
+            label="Name"
+            control={control}
+            required
+            fullWidth
+            helperText=" "
+          />
+          <TextFieldElement
+            name="email"
+            label="E-mail"
+            control={control}
+            required
+            fullWidth
+            helperText=" "
+          />
+          <PasswordElement
+            name="firstPassword"
+            label="Password"
+            control={control}
+            helperText=" "
+          />
+          <PasswordRepeatElement
+            name="secondPassword"
+            label="Password Repeat"
+            passwordFieldName="firstPassword"
+            control={control}
+            helperText=" "
+          />
+          <Button
+            sx={{
+              '&&': {
+                width: '15%',
+                minWidth: '100px',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+              },
+            }}
+            variant="contained"
+            type="submit"
+            disabled={!isValid}
+          >
+            Submit
+          </Button>
+        </Stack>
       </form>
-    </main>
+    </section>
   );
 }
 

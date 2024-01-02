038d4e41ea6cff29a58fa38691592c5895d79703
rpg0007 <zzkostyazz25@mail.ru>
2024-01-02 21:35:07 +0300

    GT-35 <feat:add my avatar and bio>

--- src/data/teamData.ts
+++ src/data/teamData.ts
@@ -1,5 +1,6 @@
 import avatar from '../assets/img/avatar.png';
 import baranovdvAvatar from '../assets/img/baranovdv.jpg';
+import rpg0007avatar from '../assets/img/RPGavatar.jpg';
 import { TeamMemberCardProps } from '../interfaces/interfaces';
 
 const teamData: TeamMemberCardProps[] = [
@@ -11,9 +12,9 @@ const teamData: TeamMemberCardProps[] = [
   },
   {
     name: 'Konstantin',
-    img: avatar,
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-    github: 'https://github.com/baranovdv/',
+    img: rpg0007avatar,
+    bio: 'I decided to become FrontEnd developer and maybe a FullStack later on.I enrolled in RSSchool React Course in October 2023, also finished Stage 1/2 of RSS FrontEnd Course 2022/2023. Had a fun time and took a lot of knowledge in.',
+    github: 'https://github.com/rpg0007/',
   },
   {
     name: 'Aleksandr',
